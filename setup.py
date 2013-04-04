f1e06fbc8412e203b4f095457abf83ba9d6ef29a
Dean Gardiner <gardiner91@gmail.com>
2013-04-04 22:18:26 +1300

    Initial Import

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='Logr',
+      packages=['logr'],
+      version='0.1',
+      description='Simple python logging wrapper',
+      author='Dean Gardiner',
+      author_email='gardiner91@gmail.com',
+      url='http://github.com/fuzeman/Logr',
+      classifiers=[
+          'Programming Language :: Python',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: OS Independent',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Logging'
+      ])
